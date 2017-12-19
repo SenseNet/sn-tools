45297ce43c00472eb1fef96e3e6eeff4fa4104ac
Zoltan Gyebrovszki <gyeby5@gmail.com>
2017-12-19 02:36:19 +0100

    Trace-analysis tests (really).

--- src/SenseNet.Tools.Tests/SnTraceAnalysisTests.cs
+++ src/SenseNet.Tools.Tests/SnTraceAnalysisTests.cs
@@ -14,136 +14,192 @@ namespace SenseNet.Tools.Tests
     [TestClass]
     public class SnTraceAnalysisTests : SnTraceTestClass
     {
-        //[TestMethod]
-        //public void SnTrace_Analysis_SimpleApi_Filter()
-        //{
-        //    CleanupAndEnableAll();
-        //    WriteStructure1();
+        [TestMethod]
+        public void SnTrace_Analysis_Filter()
+        {
+            // arrange
+            var trace = new List<string>();
+            using (UseTestWriter(trace))
+            {
+                SnTrace.EnableAll();
+                WriteStructure1();
+                SnTrace.DisableAll();
+            }
 
-        //    using (var logFlow = Reader.Create(base.DetailedLogDirectory))
-        //    {
-        //        var transformedLogFlow = logFlow
-        //            .Where(e => e.Status == Status.End)
-        //            .Where(e => e.Category == Category.Test);
+            using (var logFlow = Reader.Create(trace))
+            {
+                // action
+                var transformedLogFlow = logFlow
+                    .Where(e => e.Status == Status.End)
+                    .Where(e => e.Category == Category.Test);
 
-        //        var actual = string.Join(", ", transformedLogFlow.Select(e => e.Message));
-        //        Assert.AreEqual("10, 7, 14", actual);
-        //    }
-        //}
-        //private void WriteStructure1()
-        //{
-        //    SnTrace.Test.Write("1");
-        //    SnTrace.Write("2");
-        //    SnTrace.Test.Write("3");
-        //    SnTrace.Write("4");
-        //    SnTrace.Web.Write("5");
-        //    SnTrace.Repository.Write("6");
-        //    using (var op = SnTrace.Test.StartOperation("7"))
-        //    {
-        //        using (var op1 = SnTrace.StartOperation("8"))
-        //        {
-        //            op1.Successful = true;
-        //        }
-        //        using (var op2 = SnTrace.Repository.StartOperation("9"))
-        //        {
-        //            op2.Successful = true;
-        //        }
-        //        using (var op2 = SnTrace.Test.StartOperation("10"))
-        //        {
-        //            op2.Successful = true;
-        //        }
-        //        using (var op2 = SnTrace.Test.StartOperation("11"))
-        //        {
-        //            op2.Successful = false;
-        //        }
-        //        op.Successful = true;
-        //    }
-        //    SnTrace.Test.Write("12");
-        //    SnTrace.Repository.Write("13");
-        //    using (var op2 = SnTrace.Test.StartOperation("14"))
-        //    {
-        //        op2.Successful = true;
-        //    }
-        //    SnTrace.Repository.Write("15");
-        //    SnTrace.Test.Write("16");
+                //  assert
+                var actual = string.Join(", ", transformedLogFlow.Select(e => e.Message));
+                Assert.AreEqual("10, 7, 14", actual);
+            }
+        }
+        private void WriteStructure1()
+        {
+            SnTrace.Test.Write("1");
+            SnTrace.Write("2");
+            SnTrace.Test.Write("3");
+            SnTrace.Write("4");
+            SnTrace.Web.Write("5");
+            SnTrace.Repository.Write("6");
+            using (var op = SnTrace.Test.StartOperation("7"))
+            {
+                using (var op1 = SnTrace.StartOperation("8"))
+                {
+                    op1.Successful = true;
+                }
+                using (var op2 = SnTrace.Repository.StartOperation("9"))
+                {
+                    op2.Successful = true;
+                }
+                using (var op2 = SnTrace.Test.StartOperation("10"))
+                {
+                    op2.Successful = true;
+                }
+                using (var op2 = SnTrace.Test.StartOperation("11"))
+                {
+                    op2.Successful = false;
+                }
+                op.Successful = true;
+            }
+            SnTrace.Test.Write("12");
+            SnTrace.Repository.Write("13");
+            using (var op2 = SnTrace.Test.StartOperation("14"))
+            {
+                op2.Successful = true;
+            }
+            SnTrace.Repository.Write("15");
+            SnTrace.Test.Write("16");
 
-        //    SnTrace.Flush();
-        //}
-
-
-        //private void WriteStructure2()
-        //{
-        //    SnTrace.Write("noise"); Wait(1);
-        //    SnTrace.Write("noise"); Wait(1);
-        //    SnTrace.Test.Write("noise"); Wait(1);
-        //    SnTrace.Write("noise"); Wait(1);
-
-        //    SnTrace.Test.Write("TEST START"); Wait(1);               // relevant
-
-        //    SnTrace.Write("noise"); Wait(1);
-        //    SnTrace.Test.Write("noise"); Wait(1);
-        //    SnTrace.Write("noise"); Wait(1);
-        //    using (var op1 = SnTrace.Test.StartOperation("Op1"))
-        //    {
-        //        SnTrace.Test.Write("noise"); Wait(1);
-        //        SnTrace.Write("noise"); Wait(1);
-        //        SnTrace.Test.Write("noise"); Wait(1);
-        //        op1.Successful = true;
-        //    }                                                        // relevant
-        //    SnTrace.Write("noise"); Wait(1);
-        //    using (var op2 = SnTrace.Test.StartOperation("Op2"))
-        //    {
-        //        SnTrace.Test.Write("noise"); Wait(1);
-        //        SnTrace.Write("noise"); Wait(1);
-        //        SnTrace.Test.Write("noise"); Wait(1);
-        //        op2.Successful = true;
-        //    }                                                        // relevant
-
-        //    SnTrace.Test.Write("TEST END"); Wait(1);
-
-        //    SnTrace.Write("noise"); Wait(1);
-        //    SnTrace.Test.Write("noise"); Wait(1);
-
-        //    SnTrace.Test.Write("TEST START"); Wait(1); // (irrelevant)
-
-        //    SnTrace.Flush();
-        //}
-        //private void Wait(int milleseconds)
-        //{
-        //    System.Threading.Thread.Sleep(milleseconds);
-        //}
-
-        //[TestMethod]
-        //public void SnTrace_Analysis_FluentApi_FunctionalStyle()
-        //{
-        //    CleanupAndEnableAll();
-        //    WriteStructure2();
-
-        //    var output = new StringBuilder();
-        //    using (var writer = new StringWriter(output))
-        //    using (var logFlow = Reader.Create(base.DetailedLogDirectory))
-        //    {
-        //        var transformedLogFlow = logFlow
-        //            .Where<Entry>(e => e.Message.StartsWith("TEST ") || e.Status == Status.End)
-        //            .Take(3);
-        //        foreach (var line in transformedLogFlow)
-        //            writer.WriteLine(line);
-        //    }
-
-        //    var times = output.ToString().Split('\t').Select(TimeSpan.Parse).ToArray();
-        //    Assert.AreEqual(3, times.Length);
-        //    Assert.IsTrue(times[0] + times[1] < times[2]);
-        //}
+            SnTrace.Flush();
+        }
 
 
-        //private class TestEntry : Entry
-        //{
-        //    public TimeSpan Op1Duration { get; set; }
-        //    public TimeSpan Op2Duration { get; set; }
-        //    public TimeSpan GeneralDuration { get; set; }
+        private class AppDomainSimplifier
+        {
+            private readonly string _format;
+            private List<string> _keys = new List<string>();
 
-        //    public TestEntry(Entry sourceEntry) : base(sourceEntry) { }
-        //}
+            public AppDomainSimplifier(string format = null)
+            {
+                _format = format ?? "App-{0}";
+            }
+
+            public string Simplify(string key)
+            {
+                var i = _keys.IndexOf(key);
+                if (i < 0)
+                {
+                    i = _keys.Count;
+                    _keys.Add(key);
+                }
+                return string.Format(_format, (i + 1));
+            }
+        }
+        private class WebRequestEntryCollection : EntryCollection
+        {
+            public static class Q
+            {
+                public const string Start = "start";
+                public const string End = "end";
+            }
+
+            public Entry StartEntry;
+            public Entry EndEntry;
+
+            public override void Add(Entry e, string qualification)
+            {
+                switch (qualification)
+                {
+                    case Q.Start:
+                        StartEntry = e;
+                        break;
+                    case Q.End:
+                        EndEntry = e;
+                        break;
+                }
+            }
+
+            public override bool Finished()
+            {
+                return StartEntry != null && EndEntry != null;
+            }
+        }
+        #region string[] _logForSimpleCollectTest
+        string[] _logForSimpleCollectTest = new[]
+        {
+            ">60\t2017-11-13 03:55:48.49992\tSystem\tA:/LM/W3SVC/9/ROOT-1-131550188456390176\tT:5\t\t\t\tCPU: 0%, RAM: 680960 KBytes available (working set: 258744320 bytes)",
+            ">61\t2017-11-13 03:55:53.51096\tWeb\tA:/LM/W3SVC/9/ROOT-1-131550188456390176\tT:18\t\t\t\tPCM.OnEnter GET http://snbweb01.sn.hu/",
+            "62\t2017-11-13 03:55:53.51096\tWeb\tA:/LM/W3SVC/9/ROOT-1-131550188456390176\tT:18\t\t\t\tHTTP Action.ActionType: DefaultHttpAction, TargetNode: [null], AppNode: [null], RequestUrl:http://snbweb01.sn.hu/",
+            "63\t2017-11-13 03:55:53.52746\tWeb\tA:/LM/W3SVC/9/ROOT-1-131550188456390176\tT:18\t\t\t\tPortalAuthenticationModule.OnEndRequest. Url:http://snbweb01.sn.hu/, StatusCode:200",
+            "64\t2017-11-13 03:55:53.52746\tWeb\tA:/LM/W3SVC/9/ROOT-1-131550188456390176\tT:18\t\t\t\tPCM.OnEndRequest GET http://snbweb01.sn.hu/",
+            ">65\t2017-11-13 03:55:53.65160\tWeb\tA:/LM/W3SVC/9/ROOT-1-131550188456390176\tT:19\t\t\t\tPCM.OnEnter GET http://snbweb01.sn.hu/favicon.ico",
+            "66\t2017-11-13 03:55:53.65160\tWeb\tA:/LM/W3SVC/9/ROOT-1-131550188456390176\tT:19\t\t\t\tHTTP Action.ActionType: DefaultHttpAction, TargetNode: [null], AppNode: [null], RequestUrl:http://snbweb01.sn.hu/favicon.ico",
+            "67\t2017-11-13 03:55:53.74574\tWeb\tA:/LM/W3SVC/9/ROOT-1-131550188456390176\tT:19\t\t\t\tPortalAuthenticationModule.OnEndRequest. Url:http://snbweb01.sn.hu/favicon.ico, StatusCode:200",
+            "68\t2017-11-13 03:55:53.74574\tWeb\tA:/LM/W3SVC/9/ROOT-1-131550188456390176\tT:19\t\t\t\tPCM.OnEndRequest GET http://snbweb01.sn.hu/favicon.ico",
+            ">69\t2017-11-13 03:55:58.50896\tSystem\tA:/LM/W3SVC/9/ROOT-1-131550188456390176\tT:9\t\t\t\tCPU: 3,922418%, RAM: 570948 KBytes available (working set: 256319488 bytes)",
+        };
+        #endregion
+        [TestMethod]
+        public void SnTrace_Analysis_SimpleCollect()
+        {
+            var sb = new StringBuilder();
+            using (var writer = new StringWriter(sb))
+            using (var logFlow = Reader.Create(_logForSimpleCollectTest))
+            {
+                var aps = new AppDomainSimplifier("App{0}");
+
+                var transformedLogFlow = logFlow
+                    .Where(e => e.Category == "Web")
+                    .Select(e => { e.AppDomain = aps.Simplify(e.AppDomain); return e; })
+                    .Collect<Entry, WebRequestEntryCollection>((e) =>
+                    {
+                        if (e.Message.StartsWith("PCM.OnEnter "))
+                            return new Tuple<string, string>($"{e.AppDomain}|{e.ThreadId}|{e.Message.Substring("PCM.OnEnter ".Length)}", WebRequestEntryCollection.Q.Start);
+                        else if (e.Message.StartsWith("PCM.OnEndRequest "))
+                            return new Tuple<string, string>($"{e.AppDomain}|{e.ThreadId}|{e.Message.Substring("PCM.OnEndRequest ".Length)}", WebRequestEntryCollection.Q.End);
+                        return null;
+                    })
+                    .Take(50);
+                foreach (var item in transformedLogFlow)
+                {
+                    var app = item.StartEntry.AppDomain;
+                    var time = item.StartEntry.Time.ToString("HH:mm:ss.fffff");
+                    var req = item.StartEntry.Message.Substring("PCM.OnEnter ".Length);
+                    var dt = item.EndEntry.Time - item.StartEntry.Time;
+                    writer.WriteLine($"{app}\t{time}\t{dt}\t{req}");
+                }
+            }
+
+            // validate
+            var line = new string[3];
+            using (var reader = new StringReader(sb.ToString()))
+            {
+                // app   start-time      duration          request
+                // ----  --------------  ----------------  -------------------------------------
+                // App1  03:55:53.51096  00:00:00.0165000  GET http://snbweb01.sn.hu/
+                // App1  03:55:53.65160  00:00:00.0941400  GET http://snbweb01.sn.hu/favicon.ico
+                line[0] = reader.ReadLine();
+                line[1] = reader.ReadLine();
+                line[2] = reader.ReadLine();
+            }
+            Assert.IsNull(line[2]);
+            var item0 = line[0].Split('\t');
+            var item1 = line[1].Split('\t');
+
+            Assert.AreEqual("App1", item0[0]);
+            Assert.AreEqual("App1", item1[0]);
+            Assert.AreEqual("03:55:53.51096", item0[1]);
+            Assert.AreEqual("03:55:53.65160", item1[1]);
+            Assert.AreEqual("00:00:00.0165000", item0[2]);
+            Assert.AreEqual("00:00:00.0941400", item1[2]);
+            Assert.AreEqual("GET http://snbweb01.sn.hu/", item0[3]);
+            Assert.AreEqual("GET http://snbweb01.sn.hu/favicon.ico", item1[3]);
+        }
 
     }
 }
