d4b86940866810c03fcc7a504dfdaae2e6f71793
tusmester <tusmester@gmail.com>
2018-03-17 21:23:01 +0100

    Copy ConfigurationException from the Storage project.

---
+++ src/SenseNet.Tools/Configuration/ConfigurationException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SenseNet.Configuration
+{
+    /// <summary>
+    /// The exception that is thrown when a configuration-related error occurs.
+    /// </summary>
+    [Serializable]
+    public class ConfigurationException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationException"/> class.
+        /// </summary>
+        public ConfigurationException() { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationException"/> class.
+        /// </summary>
+        public ConfigurationException(string message) : base(message) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationException"/> class.
+        /// </summary>
+        public ConfigurationException(string message, Exception inner) : base(message, inner) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationException"/> class.
+        /// </summary>
+        protected ConfigurationException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
