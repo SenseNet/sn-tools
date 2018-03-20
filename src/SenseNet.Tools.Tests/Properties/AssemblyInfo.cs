b04afbd4d89991305c48eefce4fdf814b67c3952
tusmester <tusmester@gmail.com>
2018-03-20 16:31:32 +0100

    Raise version to 2.2 and modify assembly info and nuspec files to follow the naming conventions.

--- src/SenseNet.Tools.Tests/Properties/AssemblyInfo.cs
+++ src/SenseNet.Tools.Tests/Properties/AssemblyInfo.cs
@@ -2,11 +2,16 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SenseNet.Tools.Tests")]
+#if DEBUG
+[assembly: AssemblyTitle("SenseNet.Tools.Tests (Debug)")]
+#else
+[assembly: AssemblyTitle("SenseNet.Tools.Tests (Release)")]
+#endif
+
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sense/Net Inc.")]
-[assembly: AssemblyProduct("SenseNet.Tools.Tests")]
+[assembly: AssemblyProduct("sensenet ECM")]
 [assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
@@ -14,6 +19,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("ec7fc58f-81cc-411b-92e0-079b7c39d0a5")]
 
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.1")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyInformationalVersion("2.2.0")]
--- src/SenseNet.Tools/Properties/AssemblyInfo.cs
+++ src/SenseNet.Tools/Properties/AssemblyInfo.cs
@@ -2,11 +2,16 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SenseNet.Tools")]
-[assembly: AssemblyDescription("Tools library for Sense/Net ECM")]
+#if DEBUG
+[assembly: AssemblyTitle("SenseNet.Tools (Debug)")]
+#else
+[assembly: AssemblyTitle("SenseNet.Tools (Release)")]
+#endif
+
+[assembly: AssemblyDescription("Tools library for sensenet ECM")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sense/Net Inc.")]
-[assembly: AssemblyProduct("SenseNet.Tools")]
+[assembly: AssemblyProduct("sensenet ECM")]
 [assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
@@ -14,8 +19,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("9c2fa8d5-65f8-4cad-a65d-b7cc804a821f")]
 
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.1")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyInformationalVersion("2.2.0")]
 
 [assembly: InternalsVisibleTo("SenseNet.Tools.Tests")]
