ecfb4aebfcb8519b5a8845410ae2852911980005
kavics <gyeby5@gmail.com>
2018-06-13 14:28:13 +0200

    Feature/dotnet standard (#25)
    * Port the Tools to .net standard 2.0
    
    * Change version for easier development.
    
    * Delete unnecessary ps script for creating the nuget package.
    
    * Switch on documentation file for release config.

--- src/SenseNet.Tools/Properties/AssemblyInfo.cs
+++ src/SenseNet.Tools/Properties/AssemblyInfo.cs
@@ -2,25 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-#if DEBUG
-[assembly: AssemblyTitle("SenseNet.Tools (Debug)")]
-#else
-[assembly: AssemblyTitle("SenseNet.Tools (Release)")]
-#endif
-
-[assembly: AssemblyDescription("Tools library for sensenet ECM")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sense/Net Inc.")]
-[assembly: AssemblyProduct("sensenet ECM")]
-[assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: Guid("9c2fa8d5-65f8-4cad-a65d-b7cc804a821f")]
-
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
-[assembly: AssemblyInformationalVersion("2.3.0")]
-
 [assembly: InternalsVisibleTo("SenseNet.Tools.Tests")]
--- tools/scripts/CreateNuGetPackages.ps1
+++
@@ -1,6 +0,0 @@
-﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '\..\..\src'))
-
-# delete existing packages
-Remove-Item $PSScriptRoot\*.nupkg
-
-nuget pack $srcPath\SenseNet.Tools\SenseNet.Tools.csproj -properties Configuration=Release -OutputDirectory $PSScriptRoot
