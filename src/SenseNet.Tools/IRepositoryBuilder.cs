b6382eda412b06ea687f5661eb22aeb4991849b9
tusmester <tusmester@gmail.com>
2018-07-18 10:07:00 +0200

    Add IRepoBuilder interface. (#27)

---
+++ src/SenseNet.Tools/IRepositoryBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace SenseNet.Tools
+{
+    /// <summary>
+    /// Defines methods for adding or modifying providers loaded when the repository starts.
+    /// </summary>
+    public interface IRepositoryBuilder
+    {
+        /// <summary>
+        /// Gets a previously set provider by type.
+        /// </summary>
+        T GetProvider<T>() where T : class;
+        /// <summary>
+        /// Gets a previously set provider by its name.
+        /// </summary>
+        T GetProvider<T>(string name) where T : class;
+        /// <summary>
+        /// Sets a provider by its name.
+        /// </summary>
+        void SetProvider(string providerName, object provider);
+        /// <summary>
+        /// Sets a provider by its type.
+        /// </summary>
+        void SetProvider(object provider);
+    }
+}
