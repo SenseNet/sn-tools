c2e1519794d446ef961b9dc802ac5336591d49c9
kavics <gyeby5@gmail.com>
2020-06-03 13:32:49 +0200

    Testing tools and their tests. (#47)
    * Add testing helpers.
    
    * Accessor tests.
    
    * refactor
    
    * A Swindler test.

---
+++ src/SenseNet.Tools.Tests/AccessorTests.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Xml.Schema;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.Testing;
+
+namespace SenseNet.Tools.Tests
+{
+    [TestClass]
+    public class AccessorTests
+    {
+        #region private class AccessorTestObject
+        private class AccessorTestObject
+        {
+            private static int _staticPrivateField;
+            private static int StaticPrivateProperty { get; set; }
+
+            private int _instancePrivateField;
+            private int InstancePrivateProperty { get; set; }
+
+            public static int _staticPublicField;
+            public static int StaticPublicProperty { get; set; }
+
+            public int _instancePublicField;
+            public int InstancePublicProperty { get; set; }
+
+            public AccessorTestObject()
+            {
+            }
+            private AccessorTestObject(int field)
+            {
+                _instancePrivateField = field;
+                _instancePublicField = field;
+            }
+            private AccessorTestObject(int field, int property):this(field)
+            {
+                InstancePrivateProperty = property;
+                InstancePublicProperty = property;
+            }
+            public AccessorTestObject(int field, int property, bool isPublic) : this(field, property)
+            {
+                InstancePrivateProperty = property;
+                InstancePublicProperty = property;
+            }
+
+            private static string StaticPrivateMethod(char c, int count)
+            {
+                return "StaticPrivateMethod" + new string(c, count);
+            }
+            private string InstancePrivateMethod(char c, int count)
+            {
+                return "InstancePrivateMethod" + new string(c, count);
+            }
+
+            public static string StaticPublicMethod(char c, int count)
+            {
+                return "StaticPublicMethod" + new string(c, count);
+            }
+            public string InstancePublicMethod(char c, int count)
+            {
+                return "InstancePublicMethod" + new string(c, count);
+            }
+        }
+        #endregion
+
+        /* =============================================== Access to private members */
+
+        [TestMethod]
+        public void Accessor_Private_Type_Field()
+        {
+            var objAcc = new TypeAccessor(typeof(AccessorTestObject));
+
+            var origValue = (int)objAcc.GetStaticField("_staticPrivateField");
+            objAcc.SetStaticField("_staticPrivateField", origValue + 1);
+            var actualValue = (int)objAcc.GetStaticField("_staticPrivateField");
+            Assert.AreEqual(origValue + 1, actualValue);
+
+            objAcc.SetStaticFieldOrProperty("_staticPrivateField", origValue - 1);
+            actualValue = (int)objAcc.GetStaticFieldOrProperty("_staticPrivateField");
+            Assert.AreEqual(origValue - 1, actualValue);
+        }
+        [TestMethod]
+        public void Accessor_Private_Type_Property()
+        {
+            var objAcc = new TypeAccessor(typeof(AccessorTestObject));
+
+            var origValue = (int)objAcc.GetStaticProperty("StaticPrivateProperty");
+            objAcc.SetStaticProperty("StaticPrivateProperty", origValue + 1);
+            var actualValue = (int)objAcc.GetStaticProperty("StaticPrivateProperty");
+            Assert.AreEqual(origValue + 1, actualValue);
+
+            objAcc.SetStaticFieldOrProperty("StaticPrivateProperty", origValue - 1);
+            actualValue = (int)objAcc.GetStaticFieldOrProperty("StaticPrivateProperty");
+            Assert.AreEqual(origValue - 1, actualValue);
+        }
+        [TestMethod]
+        public void Accessor_Private_Type_Method()
+        {
+            var objAcc = new TypeAccessor(typeof(AccessorTestObject));
+
+            var actualValue = (string)objAcc.InvokeStatic("StaticPrivateMethod", '*', 5);
+            Assert.AreEqual("StaticPrivateMethod*****", actualValue);
+
+            actualValue = (string)objAcc.InvokeStatic("StaticPrivateMethod", 
+                new[] {typeof(char), typeof(int)}, 
+                new object[]{'*', 3});
+
+            Assert.AreEqual("StaticPrivateMethod***", actualValue);
+        }
+
+        [TestMethod]
+        public void Accessor_Private_Object_Ctor()
+        {
+            var objAcc = new ObjectAccessor(typeof(AccessorTestObject), 12, 42);
+
+            Assert.AreEqual(12, objAcc.GetFieldOrProperty("_instancePrivateField"));
+            Assert.AreEqual(42, objAcc.GetFieldOrProperty("InstancePrivateProperty"));
+        }
+        [TestMethod]
+        public void Accessor_Private_Object_Ctor_Types()
+        {
+            var objAcc = new ObjectAccessor(typeof(AccessorTestObject),
+                new[] {typeof(int), typeof(int)}, new object[] {12, 42});
+
+            Assert.AreEqual(12, objAcc.GetFieldOrProperty("_instancePrivateField"));
+            Assert.AreEqual(42, objAcc.GetFieldOrProperty("InstancePrivateProperty"));
+        }
+
+        [TestMethod]
+        public void Accessor_Private_Object_Field()
+        {
+            var typeAcc = new ObjectAccessor(new AccessorTestObject());
+
+            var origValue = (int)typeAcc.GetField("_instancePrivateField");
+            typeAcc.SetField("_instancePrivateField", origValue + 1);
+            var actualValue = (int)typeAcc.GetField("_instancePrivateField");
+            Assert.AreEqual(origValue + 1, actualValue);
+
+            typeAcc.SetFieldOrProperty("_instancePrivateField", origValue - 1);
+            actualValue = (int)typeAcc.GetFieldOrProperty("_instancePrivateField");
+            Assert.AreEqual(origValue - 1, actualValue);
+        }
+        [TestMethod]
+        public void Accessor_Private_Object_Property()
+        {
+            var typeAcc = new ObjectAccessor(new AccessorTestObject());
+
+            var origValue = (int)typeAcc.GetProperty("InstancePrivateProperty");
+            typeAcc.SetProperty("InstancePrivateProperty", origValue + 1);
+            var actualValue = (int)typeAcc.GetProperty("InstancePrivateProperty");
+            Assert.AreEqual(origValue + 1, actualValue);
+
+            typeAcc.SetFieldOrProperty("InstancePrivateProperty", origValue - 1);
+            actualValue = (int)typeAcc.GetFieldOrProperty("InstancePrivateProperty");
+            Assert.AreEqual(origValue - 1, actualValue);
+        }
+        [TestMethod]
+        public void Accessor_Private_Object_Method()
+        {
+            var typeAcc = new ObjectAccessor(new AccessorTestObject());
+
+            var actualValue = (string)typeAcc.Invoke("InstancePrivateMethod", '*', 5);
+            Assert.AreEqual("InstancePrivateMethod*****", actualValue);
+
+            actualValue = (string)typeAcc.Invoke("InstancePrivateMethod",
+                new[] { typeof(char), typeof(int) },
+                new object[] { '*', 3 });
+
+            Assert.AreEqual("InstancePrivateMethod***", actualValue);
+        }
+
+        /* =============================================== Access to public members */
+
+        [TestMethod]
+        public void Accessor_Public_Type_Field()
+        {
+            var objAcc = new TypeAccessor(typeof(AccessorTestObject));
+
+            var origValue = (int)objAcc.GetStaticField("_staticPublicField");
+            objAcc.SetStaticField("_staticPublicField", origValue + 1);
+            var actualValue = (int)objAcc.GetStaticField("_staticPublicField");
+            Assert.AreEqual(origValue + 1, actualValue);
+
+            objAcc.SetStaticFieldOrProperty("_staticPublicField", origValue - 1);
+            actualValue = (int)objAcc.GetStaticFieldOrProperty("_staticPublicField");
+            Assert.AreEqual(origValue - 1, actualValue);
+        }
+        [TestMethod]
+        public void Accessor_Public_Type_Property()
+        {
+            var objAcc = new TypeAccessor(typeof(AccessorTestObject));
+
+            var origValue = (int)objAcc.GetStaticProperty("StaticPublicProperty");
+            objAcc.SetStaticProperty("StaticPublicProperty", origValue + 1);
+            var actualValue = (int)objAcc.GetStaticProperty("StaticPublicProperty");
+            Assert.AreEqual(origValue + 1, actualValue);
+
+            objAcc.SetStaticFieldOrProperty("StaticPublicProperty", origValue - 1);
+            actualValue = (int)objAcc.GetStaticFieldOrProperty("StaticPublicProperty");
+            Assert.AreEqual(origValue - 1, actualValue);
+        }
+        [TestMethod]
+        public void Accessor_Public_Type_Method()
+        {
+            var objAcc = new TypeAccessor(typeof(AccessorTestObject));
+
+            var actualValue = (string)objAcc.InvokeStatic("StaticPublicMethod", '*', 5);
+            Assert.AreEqual("StaticPublicMethod*****", actualValue);
+
+            actualValue = (string)objAcc.InvokeStatic("StaticPublicMethod",
+                new[] { typeof(char), typeof(int) },
+                new object[] { '*', 3 });
+
+            Assert.AreEqual("StaticPublicMethod***", actualValue);
+        }
+
+        [TestMethod]
+        public void Accessor_Public_Object_Ctor()
+        {
+            var objAcc = new ObjectAccessor(typeof(AccessorTestObject), 12, 42, true);
+
+            Assert.AreEqual(12, objAcc.GetFieldOrProperty("_instancePublicField"));
+            Assert.AreEqual(42, objAcc.GetFieldOrProperty("InstancePublicProperty"));
+        }
+        [TestMethod]
+        public void Accessor_Public_Object_Ctor_Types()
+        {
+            var objAcc = new ObjectAccessor(typeof(AccessorTestObject),
+                new[] { typeof(int), typeof(int), typeof(bool) }, 
+                new object[] { 12, 42, true });
+
+            Assert.AreEqual(12, objAcc.GetFieldOrProperty("_instancePublicField"));
+            Assert.AreEqual(42, objAcc.GetFieldOrProperty("InstancePublicProperty"));
+        }
+
+        [TestMethod]
+        public void Accessor_Public_Object_Field()
+        {
+            var typeAcc = new ObjectAccessor(new AccessorTestObject());
+
+            var origValue = (int)typeAcc.GetField("_instancePublicField");
+            typeAcc.SetField("_instancePublicField", origValue + 1);
+            var actualValue = (int)typeAcc.GetField("_instancePublicField");
+            Assert.AreEqual(origValue + 1, actualValue);
+
+            typeAcc.SetFieldOrProperty("_instancePublicField", origValue - 1);
+            actualValue = (int)typeAcc.GetFieldOrProperty("_instancePublicField");
+            Assert.AreEqual(origValue - 1, actualValue);
+        }
+        [TestMethod]
+        public void Accessor_Public_Object_Property()
+        {
+            var typeAcc = new ObjectAccessor(new AccessorTestObject());
+
+            var origValue = (int)typeAcc.GetProperty("InstancePublicProperty");
+            typeAcc.SetProperty("InstancePublicProperty", origValue + 1);
+            var actualValue = (int)typeAcc.GetProperty("InstancePublicProperty");
+            Assert.AreEqual(origValue + 1, actualValue);
+
+            typeAcc.SetFieldOrProperty("InstancePublicProperty", origValue - 1);
+            actualValue = (int)typeAcc.GetFieldOrProperty("InstancePublicProperty");
+            Assert.AreEqual(origValue - 1, actualValue);
+        }
+        [TestMethod]
+        public void Accessor_Public_Object_Method()
+        {
+            var typeAcc = new ObjectAccessor(new AccessorTestObject());
+
+            var actualValue = (string)typeAcc.Invoke("InstancePublicMethod", '*', 5);
+            Assert.AreEqual("InstancePublicMethod*****", actualValue);
+
+            actualValue = (string)typeAcc.Invoke("InstancePublicMethod",
+                new[] { typeof(char), typeof(int) },
+                new object[] { '*', 3 });
+
+            Assert.AreEqual("InstancePublicMethod***", actualValue);
+        }
+
+        /* =============================================== Swindler tests */
+
+        [TestMethod]
+        public void Swindler()
+        {
+            AccessorTestObject.StaticPublicProperty = 42;
+            try
+            {
+                using (new Swindler<int>(142,
+                    () => AccessorTestObject.StaticPublicProperty,
+                    v => AccessorTestObject.StaticPublicProperty = v))
+                {
+                    Assert.AreEqual(142, AccessorTestObject.StaticPublicProperty);
+                    throw new Exception("An exception for fault-tolerance test.");
+                }
+            }
+            catch { /* ignored */ }
+            Assert.AreEqual(42, AccessorTestObject.StaticPublicProperty);
+        }
+    }
+}
---
+++ src/SenseNet.Tools/Testing/TypeAccessor.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+// ReSharper disable once CheckNamespace
+namespace SenseNet.Testing
+{
+    /// <summary>
+    /// Exposes accessor methods for non-visible static members of a target type.
+    /// </summary>
+    public class TypeAccessor
+    {
+        private BindingFlags _publicFlags = BindingFlags.Static | BindingFlags.Public;
+        private BindingFlags _privateFlags = BindingFlags.Static | BindingFlags.NonPublic;
+
+        /// <summary>
+        /// Gets the target type.
+        /// </summary>
+        public Type TargetType { get; }
+
+        /// <summary>
+        /// Initializes a <see cref="TypeAccessor"/> instance that wraps given target type.
+        /// </summary>
+        /// <param name="target">Type to wrap.</param>
+        public TypeAccessor(Type target)
+        {
+            TargetType = target;
+        }
+        /// <summary>
+        /// Initializes a <see cref="TypeAccessor"/> instance containing the given target type.
+        /// </summary>
+        /// <param name="assemblyName">Assembly name.</param>
+        /// <param name="typeName">Fully qualified name of the desired target type.</param>
+        public TypeAccessor(string assemblyName, string typeName)
+        {
+            TargetType = GetTypeByName(assemblyName, typeName);
+        }
+        internal static Type GetTypeByName(string assemblyName, string typeName)
+        {
+            var asm = AppDomain.CurrentDomain.GetAssemblies()
+                .FirstOrDefault(a => a.GetName().Name == assemblyName);
+            if (asm == null)
+                throw new TypeLoadException("Unknown assembly: " + assemblyName);
+
+            var tName = typeName;
+            var p = tName.IndexOf("[", StringComparison.Ordinal);
+            if (p > 0)
+                tName = tName.Substring(0, p);
+
+            var type = asm.GetType(tName, false, false);
+            if (type == null)
+                throw new TypeLoadException("Unknown type: " + typeName);
+
+            return type;
+        }
+
+        /// <summary>Gets a static field's value.</summary>
+        /// <param name="fieldName">Name of the field.</param>
+        /// <returns>The value of the static field.</returns>
+        public object GetStaticField(string fieldName)
+        {
+            var field = GetField(fieldName);
+            return field.GetValue(null);
+        }
+        /// <summary>Sets a static field's value.</summary>
+        /// <param name="fieldName">Name of the field.</param>
+        /// <param name="value">Value to set.</param>
+        public void SetStaticField(string fieldName, object value)
+        {
+            var field = GetField(fieldName);
+            field.SetValue(null, value);
+        }
+        private FieldInfo GetField(string name, bool throwOnError = true)
+        {
+            var field = TargetType.GetField(name, _publicFlags) ?? TargetType.GetField(name, _privateFlags);
+            if (field == null && throwOnError)
+                throw new ApplicationException("Field not found: " + name);
+            return field;
+        }
+
+        /// <summary>Gets a static property's value.</summary>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <returns>The value of the static property.</returns>
+        public object GetStaticProperty(string propertyName)
+        {
+            var property = GetProperty(propertyName);
+            var method = property.GetGetMethod(true) ?? property.GetGetMethod(false);
+            return method.Invoke(null, null);
+        }
+        /// <summary>Sets a static property's value.</summary>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <param name="value">Value to set.</param>
+        public void SetStaticProperty(string propertyName, object value)
+        {
+            var property = GetProperty(propertyName);
+            var method = property.GetSetMethod(true) ?? property.GetSetMethod(false);
+            method.Invoke(null, new [] { value });
+        }
+        private PropertyInfo GetProperty(string name, bool throwOnError = true)
+        {
+            var property = TargetType.GetProperty(name, _publicFlags) ?? TargetType.GetProperty(name, _privateFlags);
+            if (property == null && throwOnError)
+                throw new ApplicationException("Property not found: " + name);
+            return property;
+        }
+
+        /// <summary>Gets a static field's or property's value.</summary>
+        /// <param name="memberName">Name of the field or property</param>
+        /// <returns>The value of the static field or property.</returns>
+        public object GetStaticFieldOrProperty(string memberName)
+        {
+            var field = GetField(memberName, false);
+            if (field != null)
+                return field.GetValue(null);
+
+            var property = GetProperty(memberName, false);
+            if (property == null)
+                throw new ApplicationException("Field or property not found: " + memberName);
+
+            var method = property.GetGetMethod(true) ?? property.GetGetMethod(false);
+            if (method == null)
+                throw new ApplicationException("The property does not have getter: " + memberName);
+
+            return method.Invoke(null, null);
+        }
+        /// <summary>Sets a static field's or property's value.</summary>
+        /// <param name="memberName">Name of the field or property</param>
+        /// <param name="value">Value to set.</param>
+        public void SetStaticFieldOrProperty(string memberName, object value)
+        {
+            var field = GetField(memberName, false);
+            if (field != null)
+            {
+                field.SetValue(null, value);
+                return;
+            }
+
+            var property = GetProperty(memberName, false);
+            if (property == null)
+                throw new ApplicationException("Field or property not found: " + memberName);
+
+            var method = property.GetSetMethod(true) ?? property.GetSetMethod(false);
+            if (method == null)
+                throw new ApplicationException("The property does not have setter: " + memberName);
+
+            method.Invoke(null, new [] { value });
+        }
+
+        /// <summary>Invokes a static member.</summary>
+        /// <param name="name">Name of the member.</param>
+        /// <param name="args">Arguments to the invocation.</param>
+        /// <returns>Result of invocation.</returns>
+        public object InvokeStatic(string name, params object[] args)
+        {
+            var paramTypes = args.Select(x => x.GetType()).ToArray();
+            return InvokeStatic(name, paramTypes, args);
+        }
+        /// <summary>Invokes a static member.</summary>
+        /// <param name="name">Name of the member.</param>
+        /// <param name="parameterTypes">An array of <see cref="T:System.Type" /> objects representing the number,
+        /// order, and type of the parameters for the method to invoke.</param>
+        /// <param name="args">Arguments to the invocation.</param>
+        /// <returns>Result of invocation</returns>
+        public object InvokeStatic(string name, Type[] parameterTypes, object[] args)
+        {
+            var method = TargetType.GetMethod(name, _privateFlags, null, parameterTypes, null)
+                ?? TargetType.GetMethod(name, _publicFlags, null, parameterTypes, null);
+            if (method == null)
+                throw new ApplicationException("Method not found: " + name);
+            return method.Invoke(null, args);
+        }
+
+    }
+}
