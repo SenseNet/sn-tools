45d411273e2950302b71c3325dde0c5cf7c4201a
tusmester <tusmester@gmail.com>
2020-08-17 14:46:32 +0200

    Add a generic ILogger implementation.

---
+++ src/SenseNet.Tools/Diagnostics/SnILogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace SenseNet.Diagnostics
+{
+    /// <summary>
+    /// Routes all log messages to the official .Net log interface.
+    /// </summary>
+    public class SnILogger : SnEventloggerBase
+    {
+        private readonly ILogger<SnILogger> _logger;
+        public SnILogger(ILogger<SnILogger> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override void WriteEntry(string entry, EventLogEntryType entryType, int eventId)
+        {
+            switch (entryType)
+            {
+                case EventLogEntryType.Error:
+                case EventLogEntryType.FailureAudit:
+                    _logger?.LogError(eventId, entry);
+                    break;
+                case EventLogEntryType.SuccessAudit:
+                case EventLogEntryType.Information:
+                    _logger?.LogInformation(eventId, entry);
+                    break;
+                case EventLogEntryType.Warning:
+                    _logger?.LogWarning(eventId, entry);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(entryType), entryType, null);
+            }
+        }
+    }
+}
---
+++ src/SenseNet.Tools/Diagnostics/SnILoggerTracer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SenseNet.Diagnostics
+{
+    /// <summary>
+    /// Routes all trace messages to the official .Net log interface.
+    /// </summary>
+    public class SnILoggerTracer : ISnTracer
+    {
+        private readonly ILogger<SnILoggerTracer> _logger;
+        public SnILoggerTracer(ILogger<SnILoggerTracer> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Write(string line)
+        {
+            _logger?.LogTrace(line);
+        }
+
+        public void Flush()
+        {
+            // do nothing
+        }
+    }
+}
