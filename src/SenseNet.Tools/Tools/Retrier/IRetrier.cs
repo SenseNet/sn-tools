a0e2f950e656a7c49dba4fa84e068b0adc66466f
tusmester <tusmester@gmail.com>
2022-11-16 20:19:26 +0100

    Add xml docs for retrier methods.

--- src/SenseNet.Tools/Tools/Retrier/IRetrier.cs
+++ src/SenseNet.Tools/Tools/Retrier/IRetrier.cs
@@ -9,22 +9,120 @@ namespace SenseNet.Tools
     /// </summary>
     public interface IRetrier
     {
+        /// <summary>
+        /// Retries an operation. Maximum number of retries and waiting time comes from configuration.
+        /// </summary>
+        /// <param name="action">The operation to retry.</param>
+        /// <param name="shouldRetryOnError">If an error occurs, decides whether to retry the operation based on the exception.
+        /// Parameters: the exception and the number of previous retries.</param>
         Task RetryAsync(Func<Task> action, Func<Exception, int, bool> shouldRetryOnError);
-        Task RetryAsync(Func<Task> action, Func<Exception, int, bool> shouldRetryOnError, Action<Exception, int> handleLast);
 
+        /// <summary>
+        /// Retries an operation. Maximum number of retries and waiting time comes from configuration.
+        /// </summary>
+        /// <param name="action">The operation to retry.</param>
+        /// <param name="shouldRetryOnError">If an error occurs, decides whether to retry the operation based on the exception.
+        /// Parameters: the exception and the number of previous retries.</param>
+        /// <param name="handleLast">Custom code that will be executed after the last unsuccessful retry.</param>
+        Task RetryAsync(Func<Task> action, Func<Exception, int, bool> shouldRetryOnError,
+            Action<Exception, int> handleLast);
+
+        /// <summary>
+        /// Retries an operation. Maximum number of retries and waiting time comes from configuration.
+        /// </summary>
+        /// <typeparam name="T">Type of the return value of the operation.</typeparam>
+        /// <param name="action">The operation to retry.</param>
+        /// <param name="shouldRetryOnError">If an error occurs, decides whether to retry the operation based on the exception.
+        /// Parameters: the exception and the number of previous retries.</param>
         Task<T> RetryAsync<T>(Func<Task<T>> action, Func<Exception, int, bool> shouldRetryOnError);
-        Task<T> RetryAsync<T>(Func<Task<T>> action, Func<Exception, int, bool> shouldRetryOnError, Action<T, Exception, int> handleLast);
 
-        Task<T> RetryAsync<T>(Func<Task<T>> action, Func<T, int, bool> shouldRetry, Func<Exception, int, bool> shouldRetryOnError,
+        /// <summary>
+        /// Retries an operation. Maximum number of retries and waiting time comes from configuration.
+        /// </summary>
+        /// <typeparam name="T">Type of the return value of the operation.</typeparam>
+        /// <param name="action">The operation to retry.</param>
+        /// <param name="shouldRetryOnError">If an error occurs, decides whether to retry the operation based on the exception.
+        /// Parameters: the exception and the number of previous retries.</param>
+        /// <param name="handleLast">Custom code that will be executed after the last unsuccessful retry.</param>
+        Task<T> RetryAsync<T>(Func<Task<T>> action, Func<Exception, int, bool> shouldRetryOnError,
             Action<T, Exception, int> handleLast);
 
-        Task RetryAsync(int count, int waitMilliseconds, Func<Task> action, Func<Exception, int, bool> shouldRetryOnError);
-        Task RetryAsync(int count, int waitMilliseconds, Func<Task> action, Func<Exception, int, bool> shouldRetryOnError, Action<Exception, int> handleLast);
+        /// <summary>
+        /// Retries an operation. Maximum number of retries and waiting time comes from configuration.
+        /// </summary>
+        /// <typeparam name="T">Type of the return value of the operation.</typeparam>
+        /// <param name="action">The operation to retry.</param>
+        /// <param name="shouldRetry">If no error occurs, decides whether to retry the operation based on the result.
+        /// Parameters: the result and the number of previous retries.</param>
+        /// <param name="shouldRetryOnError">If an error occurs, decides whether to retry the operation based on the exception.
+        /// Parameters: the exception and the number of previous retries.</param>
+        /// <param name="handleLast">Custom code that will be executed after the last unsuccessful retry.</param>
+        Task<T> RetryAsync<T>(Func<Task<T>> action, Func<T, int, bool> shouldRetry,
+            Func<Exception, int, bool> shouldRetryOnError,
+            Action<T, Exception, int> handleLast);
 
-        Task<T> RetryAsync<T>(int count, int waitMilliseconds, Func<Task<T>> action, Func<Exception, int, bool> shouldRetryOnError);
-        Task<T> RetryAsync<T>(int count, int waitMilliseconds, Func<Task<T>> action, Func<Exception, int, bool> shouldRetryOnError, Action<T, Exception, int> handleLast);
+        /// <summary>
+        /// Retries an operation.
+        /// </summary>
+        /// <param name="count">Maximum number of retries.</param>
+        /// <param name="waitMilliseconds">Waiting time in milliseconds between retries.</param>
+        /// <param name="action">The operation to retry.</param>
+        /// <param name="shouldRetryOnError">If an error occurs, decides whether to retry the operation based on the exception.
+        /// Parameters: the exception and the number of previous retries.</param>
+        Task RetryAsync(int count, int waitMilliseconds, Func<Task> action,
+            Func<Exception, int, bool> shouldRetryOnError);
 
-        Task<T> RetryAsync<T>(int count, int waitMilliseconds, Func<Task<T>> action, Func<T, int, bool> shouldRetry, Func<Exception, int, bool> shouldRetryOnError,
+        /// <summary>
+        /// Retries an operation.
+        /// </summary>
+        /// <param name="count">Maximum number of retries.</param>
+        /// <param name="waitMilliseconds">Waiting time in milliseconds between retries.</param>
+        /// <param name="action">The operation to retry.</param>
+        /// <param name="shouldRetryOnError">If an error occurs, decides whether to retry the operation based on the exception.
+        /// Parameters: the exception and the number of previous retries.</param>
+        /// <param name="handleLast">Custom code that will be executed after the last unsuccessful retry.</param>
+        Task RetryAsync(int count, int waitMilliseconds, Func<Task> action,
+            Func<Exception, int, bool> shouldRetryOnError, Action<Exception, int> handleLast);
+
+        /// <summary>
+        /// Retries an operation.
+        /// </summary>
+        /// <typeparam name="T">Type of the return value of the operation.</typeparam>
+        /// <param name="count">Maximum number of retries.</param>
+        /// <param name="waitMilliseconds">Waiting time in milliseconds between retries.</param>
+        /// <param name="action">The operation to retry.</param>
+        /// <param name="shouldRetryOnError">If an error occurs, decides whether to retry the operation based on the exception.
+        /// Parameters: the exception and the number of previous retries.</param>
+        Task<T> RetryAsync<T>(int count, int waitMilliseconds, Func<Task<T>> action,
+            Func<Exception, int, bool> shouldRetryOnError);
+
+        /// <summary>
+        /// Retries an operation.
+        /// </summary>
+        /// <typeparam name="T">Type of the return value of the operation.</typeparam>
+        /// <param name="count">Maximum number of retries.</param>
+        /// <param name="waitMilliseconds">Waiting time in milliseconds between retries.</param>
+        /// <param name="action">The operation to retry.</param>
+        /// <param name="shouldRetryOnError">If an error occurs, decides whether to retry the operation based on the exception.
+        /// Parameters: the exception and the number of previous retries.</param>
+        /// <param name="handleLast">Custom code that will be executed after the last unsuccessful retry.</param>
+        Task<T> RetryAsync<T>(int count, int waitMilliseconds, Func<Task<T>> action,
+            Func<Exception, int, bool> shouldRetryOnError, Action<T, Exception, int> handleLast);
+
+        /// <summary>
+        /// Retries an operation.
+        /// </summary>
+        /// <typeparam name="T">Type of the return value of the operation.</typeparam>
+        /// <param name="count">Maximum number of retries.</param>
+        /// <param name="waitMilliseconds">Waiting time in milliseconds between retries.</param>
+        /// <param name="action">The operation to retry.</param>
+        /// <param name="shouldRetry">If no error occurs, decides whether to retry the operation based on the result.
+        /// Parameters: the result and the number of previous retries.</param>
+        /// <param name="shouldRetryOnError">If an error occurs, decides whether to retry the operation based on the exception.
+        /// Parameters: the exception and the number of previous retries.</param>
+        /// <param name="handleLast">Custom code that will be executed after the last unsuccessful retry.</param>
+        Task<T> RetryAsync<T>(int count, int waitMilliseconds, Func<Task<T>> action, Func<T, int, bool> shouldRetry,
+            Func<Exception, int, bool> shouldRetryOnError,
             Action<T, Exception, int> handleLast);
     }
 }
