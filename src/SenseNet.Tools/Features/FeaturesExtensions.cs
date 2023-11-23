88bad9f32e5849c3c40e3f2780835cffdd12c132
tusmester <tusmester@gmail.com>
2023-11-23 16:14:53 +0100

    Add feature descriptor classes and interface.

---
+++ src/SenseNet.Tools/Features/FeaturesExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SenseNet.Tools.Features;
+
+// ReSharper disable once CheckNamespace
+namespace SenseNet.Extensions.DependencyInjection;
+
+public static class FeaturesExtensions
+{
+    /// <summary>
+    /// Adds a feature to the service collection.
+    /// </summary>
+    public static IServiceCollection AddFeature<TFeature>(this IServiceCollection services)
+        where TFeature : class, ISnFeature
+    {
+        services.AddSingleton<ISnFeature, TFeature>();
+        
+        return services;
+    }
+}
