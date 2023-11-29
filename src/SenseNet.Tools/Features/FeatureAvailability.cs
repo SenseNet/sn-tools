7ae7bd07884f62cf14a59fd275e3386df43b82a7
Miklós Tóth <tusmester@gmail.com>
2023-11-29 12:11:48 +0100

    Feature api (#73)
    * Add feature descriptor classes and interface.
    
    * Update ISnFeature api

---
+++ src/SenseNet.Tools/Features/FeatureAvailability.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SenseNet.Tools.Features;
+
+/// <summary>
+/// Defines feature states.
+/// </summary>
+public enum FeatureState
+{
+    Unknown,
+    Active,
+    Disabled,
+    NotConfigured,
+    Unavailable
+}
+
+/// <summary>
+/// Provides information about the availability of a feature.
+/// </summary>
+public record FeatureAvailability
+{
+    public FeatureAvailability(FeatureState state, string reason = null, DateTime lastAvailable = default)
+    {
+        State = state;
+        Reason = reason;
+        LastAvailable = lastAvailable;
+    }
+
+    /// <summary>
+    /// State of the feature.
+    /// </summary>
+    public FeatureState State { get; }
+    /// <summary>
+    /// Reason if the feature is not available.
+    /// </summary>
+    public string Reason { get; }
+    /// <summary>
+    /// Last time the feature was available.
+    /// </summary>
+    public DateTime LastAvailable { get; }
+}
---
+++ src/SenseNet.Tools/Features/ISnFeature.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace SenseNet.Tools.Features;
+
+/// <summary>
+/// Defines a feature that can provide its state.
+/// </summary>
+public interface ISnFeature
+{
+    /// <summary>
+    /// Gets the name of the feature.
+    /// </summary>
+    public string Name { get; }
+    /// <summary>
+    /// Gets the display name of the feature.
+    /// </summary>
+    public string DisplayName { get; }
+
+    /// <summary>
+    /// Gets the current state of the feature.
+    /// </summary>
+    /// <returns>A state object that provides information about the current availability of the feature.</returns>
+    public Task<FeatureAvailability> GetStateAsync();
+}
